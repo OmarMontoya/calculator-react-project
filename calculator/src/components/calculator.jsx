4936a14fb083b73363b53ec0870bcc708d5e1739
Omar Montoya <montoyaomar92@gmail.com>
2023-06-17 23:40:01 -0300

    Added various changes in logic of the results

--- calculator/src/components/calculator.jsx
+++ calculator/src/components/calculator.jsx
@@ -8,32 +8,67 @@ export const Calculator = () => {
     let globalExpressionResult = 0;
     let resultFixed = 0;
 
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState("");
 
     function digit(number) {
         setValue(value + number);
     }
-    
+
     function result() {
-        globalExpression = value;
-        setValue('');
-        globalExpressionResult = evaluate(globalExpression);
-        resultFixed = globalExpressionResult.toString().includes('.') ? globalExpressionResult.toFixed(2) : globalExpressionResult;
-        setValue(resultFixed);
+        if (value) {
+            try {
+                globalExpression = value;
+                setValue("");
+                globalExpressionResult = evaluate(globalExpression);
+                console.log(globalExpression);
+                resultFixed = globalExpressionResult.toString().includes(".")
+                    ? globalExpressionResult.toFixed(2)
+                    : globalExpressionResult;
+                setValue(resultFixed);
+            } catch (error) {
+                alert('Syntax Error - Please add some valid input')
+            }
+        } else {
+            alert("Please add some input");
+        }
     }
 
     function resetDisplay() {
-        setValue('')
+        setValue("");
     }
 
     return (
         <>
-            <div className="main-container flex justify-center items-center flex-col w-60 h-80 border border-solid border-gray-400 rounded-md mb-60 shadow-2xl backdrop-blur-sm bg-white/30">
+            <div className="main-container flex justify-center items-center flex-col w-60 h-100 border border-solid border-gray-400 rounded-md mb-60 shadow-2xl backdrop-blur-sm bg-white/30">
                 <Display
-                    className={"input input-bordered rounded-md w-5/6 mt-5 font-bold font-sans text-4xl flex justify-end"}
+                    className={
+                        "input input-bordered rounded-md w-5/6 mt-5 font-bold font-sans text-2xl flex justify-end items-center"
+                    }
                     values={value}
                 />
                 <div className="keyboard flex flex-col gap-1 mt-5 mb-5">
+                    <div className="keyboard-container flex flex-row gap-1">
+                        <ButtonKeyboard
+                            className="btn btn-outline w-12 font-bold text-xl"
+                            htmlContent="("
+                            handlerEvent={() => digit("(")}
+                        />
+                        <ButtonKeyboard
+                            className="btn btn-outline w-12 font-bold text-xl"
+                            htmlContent=")"
+                            handlerEvent={() => digit(")")}
+                        />
+                        <ButtonKeyboard
+                            className="btn btn-outline w-12 font-bold text-xl"
+                            htmlContent="%"
+                            handlerEvent={() => digit("%")}
+                        />
+                        <ButtonKeyboard
+                            className="btn btn-outline w-12 font-bold text-xl"
+                            htmlContent="+"
+                            handlerEvent={() => digit("+")}
+                        />
+                    </div>
                     <div className="keyboard-container flex flex-row gap-1">
                         <ButtonKeyboard
                             className="btn w-12 text-xl"
@@ -52,8 +87,8 @@ export const Calculator = () => {
                         />
                         <ButtonKeyboard
                             className="btn btn-outline w-12 font-bold text-xl"
-                            htmlContent="+"
-                            handlerEvent={() => digit("+")}
+                            htmlContent="-"
+                            handlerEvent={() => digit("-")}
                         />
                     </div>
                     <div className="keyboard-container flex flex-row gap-1">
@@ -74,8 +109,8 @@ export const Calculator = () => {
                         />
                         <ButtonKeyboard
                             className="btn btn-outline w-12 font-bold text-xl"
-                            htmlContent="-"
-                            handlerEvent={() => digit("-")}
+                            htmlContent="*"
+                            handlerEvent={() => digit("*")}
                         />
                     </div>
                     <div className="keyboard-container flex flex-row gap-1">
@@ -96,15 +131,15 @@ export const Calculator = () => {
                         />
                         <ButtonKeyboard
                             className="btn btn-outline w-12 font-bold text-xl"
-                            htmlContent="x"
-                            handlerEvent={() => digit("*")}
+                            htmlContent="/"
+                            handlerEvent={() => digit("/")}
                         />
                     </div>
                     <div className="keyboard-container flex flex-row gap-1">
                         <ButtonKeyboard
                             className="btn btn-error btn-outline w-12 font-bold text-xl"
                             htmlContent="C"
-                            handlerEvent={()=>resetDisplay()}
+                            handlerEvent={() => resetDisplay()}
                         />
                         <ButtonKeyboard
                             className="btn w-12 text-xl"
@@ -113,13 +148,13 @@ export const Calculator = () => {
                         />
                         <ButtonKeyboard
                             className="btn btn-primary btn-outline w-12 font-bold text-xl"
-                            htmlContent="="
-                            handlerEvent={()=>result()}
+                            htmlContent="."
+                            handlerEvent={() => digit(".")}
                         />
                         <ButtonKeyboard
                             className="btn btn-outline w-12 font-bold text-xl"
-                            htmlContent="/"
-                            handlerEvent={() => digit("/")}
+                            htmlContent="="
+                            handlerEvent={() => result()}
                         />
                     </div>
                 </div>
